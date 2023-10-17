1e9549f867b9f5409481ea5f06312e486d0e7758
AMMHasan <mahedi.bioinf@gmail.com>
2023-10-18 00:06:38 +0100

    generate TP binary matrix

---
+++ R/collect_TPs.R
@@ -0,0 +1,69 @@
+#' create Transition Point binary matrics
+#'
+#' @param BAM_path path of the .bam files
+#' @param bin_size bin size to calculate copy number by QDNAseq
+#' @param pattern_PEA pattern for the project samples
+#' @importFrom magrittr %>%
+#' @import ACE
+#' @import Biobase
+#' @import BSgenome.Hsapiens.UCSC.hg19
+#' @import CGHbase
+#' @import CGHcall
+#' @import tidyverse
+#' @import dplyr
+#' @import magrittr
+#' @import QDNAseq
+#' @import QDNAseq.hg19
+#' @import splitstackshape
+#' @return a binary matrix
+#' @export
+#' @examples
+#' BAM_path <- "~/Documents/Research/prostate_cancer/PEACE/samples/tumour_BAM_subset/"
+#' bin_size=500
+#' pattern_PEA = "PEA310"
+#' generate_copyNumberCalled_obj(BAM_path,pattern_PEA, bin_size)
+#' 
+generate_TP_binary_matrix <- function(BAM_path,pattern_PEA, bin_size){
+  copyNumbersCalled_obj <- generate_copyNumberCalled_obj(BAM_path,pattern_PEA, bin_size)
+  
+  name_tumour_samples <- list.files(BAM_path, pattern = c(paste0(pattern_PEA,"_1"))) %>% 
+    gsub(".bam.bai", "", x=.) %>% 
+    gsub(".bam", "", x=.)  %>% 
+    unique()
+  name_plasma_samples <- list.files(BAM_path, pattern = c(paste0(pattern_PEA,"_2"))) %>% 
+    gsub(".bam.bai", "", x=.) %>% 
+    gsub(".bam", "", x=.)  %>% 
+    unique()
+  
+  
+  TP_list <- list()
+  
+  for( i in name_tumour_samples){
+    test_df <- ACE::getadjustedsegments(ACE::objectsampletotemplate(copyNumbersCalled_obj, index = i), 
+                                        cellularity = ACE::squaremodel((ACE::objectsampletotemplate(copyNumbersCalled_obj, index = i)))[["minimadf"]]$cellularity[1], 
+                                        ploidy = ACE::squaremodel((ACE::objectsampletotemplate(copyNumbersCalled_obj, index = i)))[["minimadf"]]$ploidy[1]) %>% 
+      dplyr::select(Chromosome, Start, End,Segment_Mean, Copies) %>% 
+      dplyr::mutate(TP_S = paste0(Chromosome,":",Start),TP_E=paste0(Chromosome,":",End))
+    TP_list[[i]] <- rbind(test_df %>% dplyr::select(TP=TP_S),
+                          test_df %>% dplyr::select(TP=TP_E) ) %>% 
+      unlist(use.names = F) %>% 
+      unique()
+  }
+  
+  for( i in name_plasma_samples){
+    test_df <- getadjustedsegments(ACE::objectsampletotemplate(copyNumbersCalled_obj, index = i), 
+                                   cellularity = ACE::squaremodel((ACE::objectsampletotemplate(copyNumbersCalled_obj, index = i)))[["minimadf"]]$cellularity[1], 
+                                   ploidy = ACE::squaremodel((ACE::objectsampletotemplate(copyNumbersCalled_obj, index = i)))[["minimadf"]]$ploidy[1]) %>% 
+      dplyr::select(Chromosome, Start, End,Segment_Mean, Copies) %>% 
+      dplyr::mutate(TP_S = paste0(Chromosome,":",Start),TP_E=paste0(Chromosome,":",End))
+    TP_list[[i]] <- rbind(test_df %>% dplyr::select(TP=TP_S),
+                          test_df %>% dplyr::select(TP=TP_E) ) %>% 
+      unlist(use.names = F) %>% 
+      unique()
+  }
+  
+  TP_binary_matrix <- t(splitstackshape:::charMat(listOfValues = TP_list, fill = 0L))
+  colnames(TP_binary_matrix) <- names(TP_list)
+  
+  return(TP_binary_matrix)
+}
---
+++ R/test2.R
@@ -0,0 +1,132 @@
+#' generate copy number call object
+#'
+#' @param BAM_path path of .bam files
+#' @param pattern_PEA pattern for the project samples
+#' @param bin_size bin size to calculate copy number by QDNAseq
+#' @importFrom magrittr %>%
+#' @name %>%
+#' @rdname pipe
+#' @import ACE
+#' @import Biobase
+#' @import BSgenome.Hsapiens.UCSC.hg19
+#' @import CGHbase
+#' @import CGHcall
+#' @import tidyverse
+#' @import dplyr
+#' @import magrittr
+#' @import QDNAseq
+#' @import QDNAseq.hg19
+#' @return a QDNAseq object
+#' @export
+#'
+#' @examples
+#' BAM_path <- "~/Documents/Research/prostate_cancer/PEACE/samples/tumour_BAM_subset/"
+#' bin_size=500
+#' pattern_PEA = "PEA310"
+#' generate_copyNumberCalled_obj(BAM_path,pattern_PEA, bin_size)
+#' 
+#' 
+#'
+generate_copyNumberCalled_obj <- function(BAM_path,pattern_PEA, bin_size){
+  
+  sampleFileNames <- setdiff(
+    list.files(path = BAM_path, 
+               pattern = pattern_PEA), 
+    list.files(path = BAM_path, 
+               pattern = ".bai")
+  )
+  
+  
+  bamFiles <- file.path(BAM_path, sampleFileNames)
+  
+  
+  bins <- QDNAseq::getBinAnnotations(binSize=bin_size) 
+  
+  readCounts <- QDNAseq::binReadCounts(
+    bins = bins,
+    bamfiles=bamFiles, 
+    path=NULL, # as the bamfiles contain the full path 
+    ext="bam", 
+    bamnames=NULL, 
+    phenofile=NULL,
+    chunkSize=10000000, 
+    cache=TRUE, 
+    isPaired=NA,
+    isProperPair=NA, 
+    isUnmappedQuery=FALSE, 
+    hasUnmappedMate=NA, 
+    isMinusStrand=NA,
+    isMateMinusStrand=NA, 
+    isFirstMateRead=NA, 
+    isSecondMateRead=NA, 
+    isSecondaryAlignment=NA,
+    isNotPassingQualityControls=FALSE, 
+    isDuplicate=FALSE, 
+    minMapq=37, 
+    pairedEnds=NULL
+  )
+  
+  
+  readCountsFiltered <- QDNAseq::applyFilters(
+    readCounts, 
+    residual=TRUE, 
+    blacklist=TRUE,
+    mappability=NA, 
+    bases=NA,
+    chromosomes=c("Y", "MT")
+  )
+  
+  readCountsFiltered <- QDNAseq::estimateCorrection(
+    readCountsFiltered, 
+    span=0.65, 
+    family="symmetric", 
+    adjustIncompletes=TRUE,
+    maxIter=1, 
+    cutoff=4, 
+    variables=c("gc", "mappability")
+  )
+  
+  copyNumbers <- QDNAseq::correctBins(readCountsFiltered)
+  
+  copyNumbersNormalized <- QDNAseq::normalizeBins(
+    copyNumbers,
+    method="median", 
+    force=FALSE)
+  
+  
+  copyNumbersSmooth <- QDNAseq::smoothOutlierBins(
+    copyNumbersNormalized,
+    logTransform=TRUE, 
+    force=FALSE)
+  
+  copyNumbersSegmented <- QDNAseq::segmentBins(
+    copyNumbersSmooth, 
+    transformFun="log2",
+    smoothBy=FALSE, 
+    alpha=1e-10, 
+    undo.splits="sdundo", 
+    undo.SD=1,
+    force=FALSE
+    # seeds=NULL
+  )
+  
+  
+  copyNumbersSegmented <- QDNAseq::normalizeSegmentedBins(
+    copyNumbersSegmented,
+    force = F
+  )
+  
+  
+  copyNumbersCalled <- QDNAseq::callBins(
+    copyNumbersSegmented, 
+    organism="human", 
+    method="CGHcall",
+    prior = "all"
+    #cutoffs=log2(c(deletion = 0.5, loss = 1.5, gain = 2.5, amplification = 10)/2)
+  )
+  
+  
+  return(copyNumbersCalled)
+}
+
+
